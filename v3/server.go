e9593316249c5e43885aab122893f9fe016051e4
konradfoulk <konrad.foulk@gmail.com>
2025-11-10 19:33:37 -0500

    get rooms

--- v3/server.go
+++ v3/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -19,4 +21,43 @@ type WSMessage struct {
 	Username string   `json:"username,omitempty"`
 	Content  string   `json:"content,omitempty"`
 	Rooms    []string `json:"rooms,omitempty"`
+}
+
+func main() {
+	hub := makeHub()
+	go hub.run()
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	// API endpoints
+	// get rooms (on app load)
+	http.HandleFunc("/api/rooms", func(w http.ResponseWriter, r *http.Request) {
+		getRooms(hub, w, r)
+	})
+
+	// create and delete rooms
+	http.HandleFunc("/api/rooms/", func(w http.ResponseWriter, r *http.Request) {
+		// handleRooms(hub, w, r)
+	})
+
+	// WS endpoint
+	// send messages, join and leave instructions, and changes of state (rooms created or deleted, need to update frontend)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// handleWS(hub, w, r)
+	})
+
+	// start server
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getRooms(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	roomNames := []string{}
+	for name := range hub.rooms {
+		roomNames = append(roomNames, name)
+	}
+
+	json.NewEncoder(w).Encode(roomNames)
 }
